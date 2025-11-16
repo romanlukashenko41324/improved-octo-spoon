e9068ae715ca694f338dd7b258c0a741ea5738c4
romanlukashenko41324 <roman.lukashenko.1999@gmail.com>
2025-11-16 18:51:22 +0200

    Add commented-out code examples in 2-d_lesson.py

---
+++ 2-d_lesson.py
@@ -0,0 +1,24 @@
+#def first_function(var_1,var_2,var_3=3):
+#
+#    return var_1*0.5 + var_2/3 + var_3 **4.3
+#function_res_1 = first_function(1,2)
+#print(function_res_1)
+
+# from random import randint
+#
+# def coin_simulator():
+#     coin = randint (0, 1)
+#     if coin == 0:
+#         print("No")
+#     else:
+#         print("Yes")
+# coin_simulator()
+
+# var_1=5
+# def func_1():
+#     var_1=10
+#     def second():
+#         print(var_1)
+#     second()
+# func_1()
+# print(var_1)
